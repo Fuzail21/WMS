ff71b88e53349498fb94ead24775ebc61af9d937
Fuzail Ahmed Siddiqui <121792545+Fuzail21@users.noreply.github.com>
2023-10-25 13:05:18 +0500

    Updated database.php

--- config/database.php
+++ config/database.php
@@ -80,12 +80,12 @@ return [
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
+            'url' => env('DATABASE_URL', 'sqlsrv:server=tcp:wms-laravel-server.database.windows.net,1433;database=wms-laravel-database;Uid=wms-laravel-server-admin;Pwd=BGQ14S2W1KY2I30I$;'),
+            'host' => env('DB_HOST', 'wms-laravel-server.database.windows.net'),
             'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'wms-laravel-database'),
+            'username' => env('DB_USERNAME', 'wms-laravel-server-admin'),
+            'password' => env('DB_PASSWORD', 'BGQ14S2W1KY2I30I$'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
